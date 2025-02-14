f5e323bbab261c70f9fbec1b9af25c5c7fd51cc8
NemanHajiyev <zaraganq@gmail.com>
2025-02-14 15:20:01 +0400

    Css updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { MdCall } from 'react-icons/md'
+import './App.css';
+import image from './Images/image.png';
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
+  const startCall = () => { }
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 700,
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    p: 4,
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <nav className='navbar'>
+        <div className='nav-left'>
+          <MdCall size={25} />
+          <h2>Callify</h2>
+        </div>
+        <div className='nav-center'>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Our Service</li>
+            <li>Portfolio</li>
+            <li>Contact US</li>
+          </ul>
+        </div>
+        <div className='nav-right'>
+          <button>Sign Up</button>
+          <button style={{ backgroundColor: "white", color: "black" }}>Login</button>
+        </div>
+      </nav>
+
+      <main className='Main'>
+        <div className='main-left'>
+          <h1 className='title'>Video Call</h1>
+          <h3 className='subtitle'>Bridge the distance, live the moment together :)</h3>
+          <div className='main-buttons'>
+            <button
+              onClick={startCall}
+              className='call-btn'>Start Call</button>
+            <button
+              onClick={handleOpen}
+              className='learn-btn'>Learn More</button>
+          </div>
+        </div>
+        <div className='main-right'>
+          <img src={image} />
+        </div>
+
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description">
+          <Box sx={style}>
+            <h1 style={{ color: "black" }}>Video Call Guidelines</h1>
+            <p style={{ color: "black" }}>
+              <br /><br />
+              1. Make sure you have a stable internet connection.
+              A strong and stable internet connection is essential for a smooth video call. If you experience interruptions, try switching to a different network.
+              <br /><br />
+              2. Test your audio and video before the call.
+              Ensure your microphone, speakers, and camera are working properly before starting the call. This will prevent technical issues during the conversation.
+              <br /><br />
+              3. Mute your microphone when not speaking.
+              If you're in a larger group, mute your microphone when you're not talking to reduce background noise.
+              <br /><br />
+              4. Keep distractions to a minimum.
+              For the best video call experience, avoid multitasking or using your phone during the conversation. Stay focused!
+            </p>
+          </Box>
+        </Modal>
+
+      </main>
+    </div>
   )
 }
 
---
+++ src/Call.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+
+function randomID(len) {
+    let result = '';
+    if (result) return result;
+    var chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP',
+        maxPos = chars.length,
+        i;
+    len = len || 5;
+    for (i = 0; i < len; i++) {
+        result += chars.charAt(Math.floor(Math.random() * maxPos));
+    }
+    return result;
+}
+
+export function getUrlParams(
+    url = window.location.href
+) {
+    let urlStr = url.split('?')[1];
+    return new URLSearchParams(urlStr);
+}
+
+const Call = () => {
+    const roomID = getUrlParams().get('roomID') || randomID(5);
+    let myMeeting = async (element) => {
+        const appID = 683330948;
+        const serverSecret = "b988c3b52a99ba9c157e0d586df26436";
+        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, randomID(5), randomID(5));
+        const zp = ZegoUIKitPrebuilt.create(kitToken);
+        zp.joinRoom({
+            container: element,
+            sharedLinks: [
+                {
+                    name: 'Personal link',
+                    url:
+                        window.location.protocol + '//' +
+                        window.location.host + window.location.pathname +
+                        '?roomID=' +
+                        roomID,
+                },
+            ],
+            scenario: {
+                mode: ZegoUIKitPrebuilt.GroupCall,
+            }
+        });
+    };
+    return (
+        <div
+            className="myCallContainer"
+            ref={myMeeting}
+            style={{ width: '100vw', height: '100vh' }}
+        ></div>
+    )
+}
+
+export default Call
